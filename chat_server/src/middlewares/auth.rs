eaffd15f62fcf35ece67c48e6526948144de8b4b
stargaoyc <203553391@qq.com>
2026-06-12 23:28:25 +0800

    feature: support test middleware

--- chat_server/src/middlewares/auth.rs
+++ chat_server/src/middlewares/auth.rs
@@ -26,8 +26,56 @@ pub async fn verify_token(
         Err(e) => {
             let msg = format!("Invalid token: {}", e);
             warn!(msg);
-            return (StatusCode::UNAUTHORIZED, msg).into_response();
+            return (StatusCode::FORBIDDEN, msg).into_response();
         }
     }
     next.run(req).await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{AppConfig, User};
+
+    use super::*;
+    use anyhow::Result;
+    use axum::{Router, body::Body, http::Request, middleware::from_fn_with_state, routing::get};
+    use sqlx::PgPool;
+    use tower::ServiceExt;
+
+    async fn handler() -> impl IntoResponse {
+        (StatusCode::OK, "ok")
+    }
+
+    #[sqlx::test(migrations = "../migrations")]
+    async fn verify_token_middleware_should_work(pool: PgPool) -> Result<()> {
+        let config = AppConfig::load()?;
+        let state = AppState::try_new_with_pool(config, pool).await?;
+        let user = User::new(1, "test", "test@example.com");
+        let token = state.ek.sign(user)?;
+
+        let app = Router::new()
+            .route("/", get(handler))
+            .layer(from_fn_with_state(state.clone(), verify_token))
+            .with_state(state);
+
+        let request = Request::builder()
+            .uri("/")
+            .header("Authorization", format!("Bearer {}", token))
+            .body(Body::empty())?;
+        let res = app.clone().oneshot(request).await?;
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let request = Request::builder().uri("/").body(Body::empty())?;
+        let res = app.clone().oneshot(request).await?;
+        assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+
+        let request = Request::builder()
+            .uri("/")
+            .header("Authorization", "Bearer invalid_token")
+            .body(Body::empty())?;
+        let res = app.oneshot(request).await?;
+        assert_eq!(res.status(), StatusCode::FORBIDDEN);
+
+        Ok(())
+    }
 }
