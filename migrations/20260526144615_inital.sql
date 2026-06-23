c40d897962abc9b3a07a4940642d95214b56127f
stargaoyc <203553391@qq.com>
2026-06-23 22:58:50 +0800

    feature: add triggers

--- migrations/20260526144615_inital.sql
+++ migrations/20260526144615_inital.sql
@@ -3,6 +3,7 @@
 -- create user table
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT NOT NULL,
     fullname VARCHAR(64) NOT NULL UNIQUE,
     -- hashed argon2 password
     password_hash VARCHAR(97) NOT NULL,
@@ -10,7 +11,26 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
--- create index on email for user table
+-- workspace for users
+CREATE TABLE IF NOT EXISTS workspaces (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL UNIQUE,
+    owner_id BIGINT NOT NULL REFERENCES users(id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+-- add super user and default workspace
+BEGIN;
+INSERT INTO users (id, ws_id, fullname, email, password_hash)
+VALUES (0, 0, 'Super User', 'superuser@example.com', '');
+INSERT INTO workspaces (id, name, owner_id) VALUES (0, 'none', 0);
+COMMIT;
+
+-- add foreign key constraint for ws_id for users table
+ALTER TABLE users
+    ADD CONSTRAINT fk_users_ws_id FOREIGN KEY (ws_id) REFERENCES workspaces(id);
+
+---- create index on email for user table
 CREATE INDEX IF NOT EXISTS email_idx ON users (email);
 
 -- create chat type
@@ -19,6 +39,7 @@ CREATE TYPE chat_type AS ENUM ('single', 'group', 'public_channel', 'private_cha
 -- create chat table
 CREATE TABLE IF NOT EXISTS chats (
     id BIGSERIAL PRIMARY KEY,
+    ws_id BIGINT NOT NULL REFERENCES workspaces(id),
     name VARCHAR(64),
     type chat_type NOT NULL,
     members BIGINT[] NOT NULL,
---
+++ migrations/20260623143206_triggers.sql
@@ -0,0 +1,38 @@
+-- Add migration script here
+-- if chat changed, notify with chat data
+CREATE OR REPLACE FUNCTION add_to_chat()
+RETURNS TRIGGER AS $$
+BEGIN
+    RAISE NOTICE 'User added to chat: %', NEW;
+    PERFORM pg_notify('chat_updated', jsonb_build_object(
+        'op', PG_OP,
+        'old', OLD,
+        'new', NEW
+        )::TEXT);
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER add_to_chat_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON chats
+    FOR EACH ROW
+    EXECUTE FUNCTION add_to_chat();
+
+-- if new message added, notify with message data
+CREATE OR REPLACE FUNCTION add_to_message()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF PG_OP = 'INSERT' THEN
+    RAISE NOTICE 'Message added to chat: %', NEW;
+    PERFORM pg_notify('chat_message_created', row_to_jsonb(NEW)::TEXT);
+    END IF;
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER add_to_message_trigger
+    AFTER INSERT ON messages
+    FOR EACH ROW
+    EXECUTE FUNCTION add_to_message();
